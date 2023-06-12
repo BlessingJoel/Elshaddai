b612f736c16be0121fac38ec5201be85a2b71869
Ajith <ajithudangudi8@gmail.com>
2023-06-12 15:14:26 +0530

    First Commit

--- SeleniumAdvC/src/main/java/Generic_Utility/Sample.java
+++ SeleniumAdvC/src/main/java/Generic_Utility/Sample.java
@@ -12,9 +12,10 @@ public static void main(String[] args) {
 			
 	 WebDriverManager.chromedriver().setup();
       WebDriver driver=new ChromeDriver();
+      driver.get("https://docs.phptravels.com/");
       
-      webdriver_Utility us=new webdriver_Utility();
-      us.launchApplication("https://docs.phptravels.com/");
+//      webdriver_Utility us=new webdriver_Utility();
+//      us.launchApplication("https://docs.phptravels.com/");
 
 	}
 }
